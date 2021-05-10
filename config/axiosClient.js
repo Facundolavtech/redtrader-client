496f7e479510d2e3f98acb443466425af3ba021a
Facundo Lavagnino/desktop <facuhlavagninodq@gmail.com>
2021-05-10 03:29:45 -0300

    updating api endpoint

--- config/axiosClient.js
+++ config/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const productionURL = "ec2-18-231-117-23.sa-east-1.compute.amazonaws.com/api/";
+const productionURL =
+  "http://ec2-18-231-117-23.sa-east-1.compute.amazonaws.com/api/";
 const developmentURL = "http://localhost:4001/api";
 
 const axiosClient = axios.create({
