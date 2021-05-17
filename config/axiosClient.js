8fc5881e39668bba0e8374b53c0873bd2976adde
Facundo Lavagnino/desktop <facuhlavagninodq@gmail.com>
2021-05-17 19:46:37 -0300

    refactor auth method

--- config/axiosClient.js
+++ config/axiosClient.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API_ENDPOINT = process.env.API_ENDPOINTv2 || "http://localhost:4001/api";
+const API_PRODUCTION = "https://redtrader-api.com:9443/api";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:4001/api",
+  baseURL: API_PRODUCTION,
   headers: {
     "Content-Type": "application/json",
   },
