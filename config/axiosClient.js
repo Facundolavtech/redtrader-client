31e1f8516434e91541c8c8246f694258d44e7f52
Facundo Lavagnino/desktop <facuhlavagninodq@gmail.com>
2021-05-17 13:51:06 -0300

    remove next config

--- config/axiosClient.js
+++ config/axiosClient.js
@@ -2,10 +2,8 @@ import axios from "axios";
 
 const API_ENDPOINT = process.env.API_ENDPOINTv2 || "http://localhost:4001/api";
 
-// "https://redtrader-api.com:9443/api"
-
 const axiosClient = axios.create({
-  baseURL: API_ENDPOINT,
+  baseURL: "https://redtrader-api.com:9443/api",
   headers: {
     "Content-Type": "application/json",
   },
